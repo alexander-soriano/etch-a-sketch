1447521a51b36ab2f97d07784905b8c2e17f7d97
Alexander <alexander.soriano001@gmail.com>
2022-04-25 17:22:23 +0800

    created initial framework for this project

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container')
+
+function createGrid(numberOfGrid){
+    for (i=1; i<=numberOfGrid; i++){
+        const box = document.createElement('div');
+        box.classList.add(`box`);
+        container.appendChild(box);
+    }
+
+    let side = Math.sqrt(numberOfGrid);
+    const allBox = container.querySelectorAll('.box');
+    allBox.forEach(box => box.style.cssText=`height: ${100/side}%; width: ${100/side}%;`)
+
+    allBox.forEach(box => box.addEventListener('mouseover', (e) => {
+        e.target.classList.add('open');
+    }));
+    
+};
+
+createGrid(16*16);
+
+
+
+
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', (e) => {
+    container.textContent = '';
+    do {
+        square = prompt("Enter a number less than or equal 100?", 0);
+      } while (square > 100 && square);
+    createGrid(square*square)
+})
