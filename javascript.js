565818fc258b4e96644f46528a89ba7844af151c
Alexander <alexander.soriano001@gmail.com>
2022-04-26 12:57:42 +0800

    added random color & reset to black

--- javascript.js
+++ javascript.js
@@ -7,19 +7,21 @@ function createGrid(numberOfGrid){
         container.appendChild(box);
     }
 
+function adaptSquare(box){
+    box.style.cssText=`height: ${100/side}%; width: ${100/side}%;`
+}
+
     let side = Math.sqrt(numberOfGrid);
     const allBox = container.querySelectorAll('.box');
-    allBox.forEach(box => box.style.cssText=`height: ${100/side}%; width: ${100/side}%;`)
+    allBox.forEach(adaptSquare)
 
     allBox.forEach(box => box.addEventListener('mouseover', (e) => {
-        e.target.classList.add('open');
-    }));
+            e.target.classList.add('open');
+        }));    
     
 };
 
 createGrid(16*16);
-
-
 
 
 const resetButton = document.querySelector('.reset');
@@ -30,3 +32,20 @@ resetButton.addEventListener('click', (e) => {
       } while (square > 100 && square);
     createGrid(square*square)
 })
+
+
+const blackButton = document.querySelector('.black');
+blackButton.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--baseColor', 'black')
+});
+
+const randomButton = document.querySelector('.random');
+randomButton.addEventListener('click', () => {
+    function randomRGB(){
+        return Math.floor(Math.random()*255)
+    };
+    let r = randomRGB();
+    let g = randomRGB();
+    let b = randomRGB();
+    document.documentElement.style.setProperty('--baseColor', `rgb(${r} ${g} ${b} / 100%)`)
+});
